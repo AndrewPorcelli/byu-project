6a6c6100fa5bfa6c82ca7d31dcd3154760baa60e
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-07-07 11:54:16 -0600

    Fixed an id on db.json to be id 12. Required db.json on controller.js to import files

--- back-end/controller.js
+++ back-end/controller.js
@@ -1,12 +1,10 @@
 // set up the functions and buttons
 // for ticket page, use a hidden class in the html that i will create on the index.html and then once clicked, just unhide.
+const schedule = require('./db.json');
 
 
 
 //Functions
 
-
 // exporting functions
-module.exports = {
-   
-};
+module.exports = {};
